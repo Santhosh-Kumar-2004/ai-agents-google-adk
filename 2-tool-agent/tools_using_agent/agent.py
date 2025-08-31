8eebf79ce3a027455481d0e82b92981b6f6b9dd2
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-08-31 14:04:17 +0530

    tool agent Agent.py

---
+++ 2-tool-agent/tools_using_agent/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+root_agent = Agent(
+    name="tools_using_agent",
+    model="gemini-2.0-flash",
+    description="Tool Agent",
+    instruction="""
+    You can use various tools to assist with tasks. It can search the web, access databases, and perform other actions as needed. You are a helpful assistant that can use the Following tools:
+    - Google Search
+    """,
+    tools=[google_search]
+)
