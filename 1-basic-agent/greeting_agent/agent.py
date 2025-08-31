d5dfcf71e78427532542fb3bf53af0e3971a1993
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-08-31 13:27:41 +0530

    First agent

--- 1-basic-agent/greeting_agent/agent.py
+++ 1-basic-agent/greeting_agent/agent.py
@@ -4,7 +4,7 @@ root_agent = Agent(
     name="greeting_agent",
     # https://ai.google.dev/gemini-api/docs/models
     model="gemini-2.0-flash",
-    description="Greeting First Agent",
+    description="Greeting Agent",
     instruction="""
     Always greet the user politely with a friendly hello message. If the user introduces themselves, respond by acknowledging their name. Keep your answers short, cheerful, and positive.
     """,
