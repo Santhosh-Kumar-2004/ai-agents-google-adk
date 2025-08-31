978ee29fa342ced2812959d1c2da44830765c07b
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-08-31 15:04:51 +0530

    custom function usecase with agebt

--- 2-tool-agent/tool_agent/agent.py
+++ 2-tool-agent/tool_agent/agent.py
@@ -1,6 +1,6 @@
-import datetime
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+from datetime import datetime
 
 # we can create and give it as a tool to adk agents
 def get_current_time() -> dict:
@@ -17,9 +17,9 @@ root_agent = Agent(
     description="Tool Agent",
     instruction="""
     You can use various tools to assist with tasks. It can search the web, access databases, and perform other actions as needed. You are a helpful assistant that can use the Following tools:
-    - Google Search
+    - Get Current Time
     """,
-    tools=[google_search]
-    # tools=[get_current_time],
+    # tools=[google_search]
+    tools=[get_current_time],
     # tools=[google_search, get_current_time], # <--- Doesn't work
 )
